fcbba26ad61ecd5730309cc3b1e7f06b38a26b2f
Stefan Ledin <stefan.ledin@gmail.com>
2013-04-01 15:17:59 +0200

    JS kinda works

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,54 @@
-;(function ($) {
+var App = (function($) {
+
+	function getTweets () {
+		$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=k_berglund&count=5&callback=?', function(data) {
+			var data = data,
+				html = '';
+
+			for (var i = 0; i < data.length; i++) {
+				html += '<li>'+data[i].text+'</li>';
+			};
+
+			document.getElementById('tweets').innerHTML = twitterify(html);
+		});		
+	}
 	
-	// Boot Foundation-stuff
-	$(document).foundation();
+	function filterProjects () {
 
-	// Get tweets
-	$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=k_berglund&count=5&callback=?', function(data) {
-		var data = data,
-			html = '';
+		var checkboxes = document.getElementById('filter').elements;
+		
+		// Add eventlistener
+		[].forEach.call(checkboxes, function (el) {
+			
+			el.addEventListener('change', filterCategories);
 
-		for (var i = 0; i < data.length; i++) {
-			html += '<li>'+data[i].text+'</li>';
-		};
+		});
 
-		document.getElementById('tweets').innerHTML = twitterify(html);
-	});
+		function filterCategories () {
+			[].forEach.call(checkboxes, function (el) {
+				if (el.checked) {
+					[].forEach.call(document.querySelectorAll('article'+el.value), function (project) {
+						project.style.display="block";
+					});
+				} else {
+					[].forEach.call(document.querySelectorAll('article'+el.value), function (project) {
+						project.style.display="none";
+					});
+				}
+			});
+		}
+
+	}
+
+	return function () {
+		// Boot foundationstuff
+		$(document).foundation();
+
+		getTweets();
+		
+		filterProjects();
+	};
 
 }(jQuery));
+
+App();
